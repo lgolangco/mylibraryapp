4524cc463324ad2f0cf234f9c426c8e783ea25ab
lgolangco <lgolangco@student.unimelb.edu.au>
2020-04-24 10:30:55 +1000

    changed connection_string in index.js

--- models/index.js
+++ models/index.js
@@ -1,11 +1,10 @@
 require('dotenv').config()
    const mongoose = require("mongoose");
    // Connect to MongoDB --- Replace this with your Connection String
-   CONNECTION_STRING = "mongodb+srv://singhrr:<password>@singhscluster-
-   h0e35.mongodb.net/test?retryWrites=true&w=majority";
+   CONNECTION_STRING = "mongodb+srv://laurengolangco:<password>@cluster0-kpojj.mongodb.net/test?retryWrites=true&w=majority";
    MONGO_URL =
    CONNECTION_STRING.replace("<password>",process.env.MONGO_PASSWORD);
-   mongoose.connect(mongodb+srv://laurengolangco:<password>@cluster0-kpojj.mongodb.net/test?retryWrites=true&w=majority, {
+   mongoose.connect(MONGO_URL, || "mongodb://localhost/info30005", {
      useNewUrlParser: true,
      useCreateIndex: true,
      useUnifiedTopology: true,
